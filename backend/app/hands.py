463bfe6a4f9041258ab8d067d945cc51296cf9e2
pawariya <145284581+pawariya@users.noreply.github.com>
2026-03-03 15:47:18 -0600

    completed hands.py

--- backend/app/hands.py
+++ backend/app/hands.py
@@ -15,3 +15,22 @@ The Hand class does NOT:
 - Validate contracts.
 - Interact with other players.
 """
+
+class Hand:
+    def __init__(self):
+        self.cards = [] #using a arraylist
+
+    def draw_card (self, card):
+        self.card.append(card)
+
+    def discard_card(self, card):
+        self.card.remove(card)
+
+    def sort_cards(self):
+        self.card.sort(key=lambda card: (card.sort_value, card.suit_value))
+
+    def deadwood (self) -> int: # calculating remaining card pts
+        return sum (card.point_value for card in self.cards)
+    
+    def get_card(self):
+        return list(self.cards) #using list incase there are changes in the hand internal state
